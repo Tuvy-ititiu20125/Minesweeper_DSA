b79803b3c89ffc09da79fb773aa487ce600c6081
Tuvy-ititiu20125 <91869179+Tuvy-ititiu20125@users.noreply.github.com>
2023-05-11 10:36:28 +0700

    Upload file Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,10 @@
 public class Board{
-    
+     public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            var ex = new Minesweeper();
+            ex.setVisible(true);
+        });
+    } 
 }
